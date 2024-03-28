87ec2cb3ae5ff836df72fce65c9e65a92f91946e
KortoKript <163549345+KortoKript@users.noreply.github.com>
2024-03-28 13:44:34 +0300

    upd

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-const button = document.querySelector('#enter');
+const text = document.querySelector('#text');
+
+function Send(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://catfact.ninja/fact', true)
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            name.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
